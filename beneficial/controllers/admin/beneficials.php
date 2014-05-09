36525d0b0f4a0eb0e7896635b5b6fd1b4507ab68
Ruman Ranjit <rumanranjit@gmail.com>
2014-05-09 18:54:54 +0545

    beneficial ko hai

---
+++ beneficial/controllers/admin/beneficials.php
@@ -0,0 +1,109 @@
+<?php 
+
+class Beneficials extends Admin_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('beneficial/beneficial_model');
+        $this->load->model('events/event_model');
+	}
+	
+    
+	public function index()
+	{
+	    $data['view_page']='admin/index';
+        $data['header']='Beneficials';
+		$data['events']=$this->event_model->getEvents()->result_array();
+		//$data['beneficials']=$this->beneficial_model->getBeneficials()->result_array();
+		$this->load->view($this->_container,$data);
+	}
+	
+	public function lists()
+	{
+	    $partial_page='admin/lists';
+		$data['beneficials']=$this->beneficial_model->getBeneficials()->result_array();
+		$this->load->view($partial_page,$data);		
+	}
+	
+	public function detail($id=NULL)
+	{
+		if(is_null($id))
+		{
+			redirect(site_url());
+		}
+	}
+    
+    public function add()
+    {
+        $data['view_page']='beneficial/admin/add';
+        $data['header']='Add Beneficial';
+        $data['events']=$this->event_model->getEvents()->result_array();
+        $this->load->view($this->_container,$data);
+    }
+    
+    public function edit($id)
+    {
+        $data['view_page']='beneficial/admin/edit.php';
+        $beneficial=$this->beneficial_model->getBeneficials(array('beneficial_id'=>$id))->row_array();
+        $data['events']=$this->event_model->getEvents()->result_array();
+        $data['header']='Edit '.$beneficial['beneficial_name'];  
+        $data['beneficial']=$beneficial;
+        $this->load->view($this->_container,$data);
+    }
+    
+		public function save()
+	{
+		//$data=array('alert_name'=>$this->input->post('alert_name'));
+		$data=$this->_get_posted_data();
+		$success=FALSE;
+		if($data['beneficial_id']==''){
+			$success=$this->beneficial_model->insert('BENEFICIALS',$data);
+		}
+		else
+		{
+			$success=$this->beneficial_model->update('BENEFICIALS',$data,array('beneficial_id'=>$data['beneficial_id']));
+		}
+		
+		echo json_encode(array('success'=>$success));
+		
+		//redirect(site_url('admin/alert'));
+		//$this->alert_model->delete('ALERTS',array('alert_id'=>2));
+		//echo $this->db->last_query();
+	}
+    
+    public function remove($id)
+    {
+        $success=$this->beneficial_model->delete('BENEFICIALS',array('beneficial_id'=>$id));
+		echo json_encode(array('success'=>$success));
+        //redirect(site_url('admin/alert'));
+    }
+    
+    private function _get_posted_data()
+	{
+		$data['beneficial_id']=$this->input->post('beneficial_id');
+		$data['beneficial_name']=$this->input->post('beneficial_name');
+		$data['beneficial_address']=$this->input->post('beneficial_address');
+        $data['event_id']=$this->input->post('event_id');
+		$data['status']=$this->input->post('status');
+		return $data;
+	}
+	
+	public function json(){
+		$rows=$this->beneficial_model->getBeneficials()->result_array();
+	     echo json_encode($rows);
+	}
+	
+	public function getByIdJSON()
+	{
+		$id=$this->input->get('id');
+		if($id)
+		{
+			$rows=$this->alert_model->getBeneficials(array('beneficial_id'=>$id))->row_array();
+	     	echo json_encode($rows);	
+		}
+	}
+}
+
+?>
+
---
+++ beneficial/views/admin/add.php
@@ -0,0 +1,45 @@
+<body>
+<h2><span class="label label-primary">Add Beneficial</span></h2>
+<form action="<?php echo site_url('beneficial/admin/save');?>" method="post" class="navbar-form navbar-left">
+<div class="form-group">
+<table class="table">
+  <tr>
+    <td class="navbar-brand">Beneficial Name</td>
+    <td><label for="beneficial_name"></label>
+    
+      <input type="text" name="beneficial_name" id="beneficial_name" class="form-control" required autofocus></td>
+  </tr>
+  <tr>
+    <td class="navbar-brand">Beneficial Address:</td>
+    <td><label for="beneficial_address"></label>
+      <input type="text" name="beneficial_address" id="beneficial_address" cols="45" rows="5" class="form-control" required></td>
+  </tr>
+  <tr>
+  <td class="navbar-brand">Event:</td>
+  <td><select name="event_id" id="event_id" >
+    <?php
+	foreach($events as $event){
+    ?>
+    <option value="<?php echo $event['event_id'];?>" id="<?php echo $event['event_id'];?>" name="<?php echo $event['event_id'];?>"><?php echo $event['event_name'];?></option>
+    <?php
+	}
+	?></select>
+  <a href="<?php echo site_url('events/admin/events/add')?>" target="_blank">Add Event</a></td></td>
+  </tr>
+  <tr>
+    <td class="navbar-brand">Status:</td>
+    <td><input type="radio" name="status" id="status1" value="1" class="form-control"/> 
+    ON
+        <input type="radio" name="status" id="status0" value="0" class="form-control"/>OFF
+      <label for="status"></label></td>
+  </tr>
+  <tr>
+    <td class="navbar-brand">&nbsp;</td>
+    <td><input type="submit" name="submit" id="submit" value="Submit" /> 
+    <input type="button" name="notify" id="notify" value="Submit and Notify"/>
+      <a href="<?php echo site_url('admin/beneficials')?>">Cancel</a></td>
+  </tr>
+ 
+</table>
+</div>
+</form>
