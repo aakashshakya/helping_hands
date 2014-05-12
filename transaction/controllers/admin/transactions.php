0ca7f88f16bcd2dc3c9ccfa450a6e01e9376b9ce
Aakash Shakya <aakash_shakya@hotmail.com>
2014-05-12 19:28:00 +0545

    Transactions ahs been updated

--- transaction/controllers/admin/transactions.php
+++ transaction/controllers/admin/transactions.php
@@ -1,6 +1,9 @@
 <?php 
 
 class Transactions extends Admin_Controller {
+
+protected $uploadPath = 'uploads/';
+protected $uploadthumbpath= 'uploads/thumb/';
 	
 	public function __construct()
 	{
@@ -14,6 +17,7 @@ class Transactions extends Admin_Controller {
 	{
 	    $data['view_page']='admin/index.php';
         $data['header']='Transactions';
+		$data['users']=$this->user_model->getUsers()->result_array();
 		$data['transactions']=$this->transaction_model->getTransactions()->result_array();
 		$this->load->view($this->_container,$data);
 	}
@@ -80,6 +84,8 @@ class Transactions extends Admin_Controller {
 		$data['transaction_id']=$this->input->post('transaction_id');
 		$data['user_id']=$this->input->post('user_id');
 		$data['transaction_image']=$this->input->post('transaction_image');
+		print_r($data);
+		exit;
 		return $data;
 	}
 	
@@ -97,7 +103,44 @@ class Transactions extends Admin_Controller {
 	     	echo json_encode($rows);	
 		}
 	}
+
+      function upload_image(){
+		//Image Upload Config
+		$config['upload_path'] = $this->uploadPath;
+		$config['allowed_types'] = 'gif|png|jpg';
+		$config['max_size']	= '100240';
+		$config['remove_spaces']  = true;
+		//load upload library
+		$this->load->library('upload', $config);
+		if(!$this->upload->do_upload())
+		{
+			$data['error'] = $this->upload->display_errors('','');
+			echo json_encode($data);
+		}
+		else
+		{
+		  $data = $this->upload->data();
+ 		  $config['image_library'] = 'gd2';
+		  $config['source_image'] = $data['full_path'];
+          $config['new_image']    = $this->uploadthumbpath;
+		  //$config['create_thumb'] = TRUE;
+		  $config['maintain_ratio'] = TRUE;
+		  $config['height'] =100;
+		  $config['width'] = 100;
+
+		  $this->load->library('image_lib', $config);
+		  $this->image_lib->resize();
+		  echo json_encode($data);
+	    }
+	}
+	
+	function upload_delete(){
+		//get filename
+		$filename = $this->input->post('filename');
+		@unlink($this->uploadPath . '/' . $filename);
+	} 
+
 }
 
-?>
+
 
--- transaction/views/admin/index.php
+++ transaction/views/admin/index.php
@@ -11,19 +11,35 @@ Add Transaction</a>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-    <form id="transaction-form" method="post">
+    <form id="transaction-form" method="post" enctype="multipart/form-data">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <h4 class="modal-title" id="myModalLabel">Edit Transaction</h4>
       </div>
       <div class="modal-body">
-        <div class="span8">
-        <label>Transaction Image:</label>
-        <input type="text" name="transaction_image" id="transaction_image" />
+        <div class="span8"><tr>
+    <td width="86" class="navbar-brand">Image</td>
+    <td width="225"><label for="transaction_image"></label>
+    
+     
+<label id="upload_image_name" style="display:none"></label>
+                      <input name="transaction_image" id="transaction_image" type="text" style="display:none"/>
+                      <input type="file" id="upload_image" name="userfile" style="display:block"/>
+                      <a href="#" id="change-image" title="Delete" style="display:none"><img src="https://pagodalabs.com/demo/facebook_app/assets/icons/delete.png" border="0"/></a>      
+      </td>
+  </tr>
         </div>
-        <div class="span8">
-        <label>User</label>
-        <input type="text" name="user_id" id="user_id" />
+      
+        <div class="span8"><label>User:</label>
+ <select name="user_id" id="user_id" >
+    <?php
+	foreach($users as $user){
+    ?>
+    <option value="<?php echo $user['user_id'];?>" id="<?php echo $user['user_id'];?>" name="<?php echo $user['user_id'];?>"><?php echo $user['user_name'];?></option>
+    <?php
+	}
+	?></select>
+  <a href="<?php echo site_url('users/admin/users/add')?>" target="_blank">Add user</a>
         </div>
         <input type="hidden" id="transaction_id" name="transaction_id"/>     
       </div>
@@ -44,7 +60,8 @@ var transaction={
 			add:function(){
 						$('#transaction_id').val('');
 						$('#transaction_image').val('');
-						$('#user_id').val('');
+						$('#user_id').val('');						
+						$('#myModalLabel').html('Add Transaction');
 						$('#myModal').modal();	
 						$('#transaction_name').focus();
 			},
@@ -52,8 +69,8 @@ var transaction={
 			{
 				$.getJSON('<?php echo site_url('transaction/admin/transactions/getByIdJSON')?>',{id:id},function(data){
 					$('#transaction_id').val(data.transaction_id);
-					$('#transaction_name').val(data.transaction_image);
-					$('#transaction_description').val(data.user_id);
+					$('#transaction_image').val(data.transaction_image);
+					$('#user_id').val(data.user_id);
 					$('#myModal').modal();
 				});				
 			},
@@ -99,6 +116,42 @@ var transaction={
 $(document).on('ready',function()
 {
 	transaction.list();
+	uploadReady();
 });
+
+	function uploadReady()
+	{
+		uploader=$('#upload_image');
+		new AjaxUpload(uploader, {
+			action: '<?php echo site_url('transaction/admin/transactions/upload_image')?>',
+			name: 'userfile',
+			responseType: "json",
+			onSubmit: function(file, ext){
+				 if (! (ext && /^(jpg|png|jpeg|gif)$/.test(ext))){ 
+                    // extension is not allowed 
+					//$.messager.show({title: 'Error',msg: 'Only JPG, PNG or GIF files are allowed'});
+					alert('Only JPG, PNG or GIF files are allowed');
+					return false;
+				}
+				//status.text('Uploading...');
+			},
+			onComplete: function(file, response){
+				if(response.error==null){
+					var filename = response.file_name;
+					$('#upload_image').hide();
+					$('#transaction_image').val(filename);
+					$('#upload_image_name').html(filename);
+					$('#upload_image_name').show();
+					$('#change-image').show();
+				}
+                else
+                {
+					alert(response.error);
+					//$.messager.show({title: 'Error',msg: response.error});                
+                }
+			}		
+		});		
+	}
+
 
 </script>
